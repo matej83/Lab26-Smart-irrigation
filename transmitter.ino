f32f01bc597fd281d0d7bee398ee4434c6738a40
matej83 <59120612+matej83@users.noreply.github.com>
2021-11-14 21:16:37 +0100

    Update transmitter.ino
    4 sensor support

--- transmitter.ino
+++ transmitter.ino
@@ -8,8 +8,14 @@ transmitter
 
 int id = 1; // change to board id
 
-int sensorPin= 34; //A0 to esp32 GPIO 34
-int sensorValue; // value of ADC 0-4095 (0v-3.3v)
+int sensorPin1= 34; //A0 to esp32 GPIO pin 34
+int sensorPin2= 35; //A0 to esp32 GPIO pin 33
+int sensorPin3= 32; //A0 to esp32 GPIO pin 32
+int sensorPin4= 33; //A0 to esp32 GPIO pin 31
+int sensorValue1; // value of ADC 0-4095 (0v-3.3v)
+int sensorValue2; // value of ADC 0-4095 (0v-3.3v)
+int sensorValue3; // value of ADC 0-4095 (0v-3.3v)
+int sensorValue4; // value of ADC 0-4095 (0v-3.3v)
 
 // REPLACE WITH THE RECEIVER'S MAC Address
 uint8_t broadcastAddress[] = {0x24, 0x0A, 0xC4, 0x5E, 0xE8, 0x7C};
@@ -18,8 +24,10 @@ uint8_t broadcastAddress[] = {0x24, 0x0A, 0xC4, 0x5E, 0xE8, 0x7C};
 // Must match the receiver structure
 typedef struct struct_message {
     int id; // must be unique for each sender board
-    int x;
-//  int y;
+    int h1;
+    int h2;
+    int h3;
+    int h4;
 } struct_message;
 
 
@@ -34,13 +42,18 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 void readsensor(){
-  sensorValue =analogRead(sensorPin); //read sensor value (the wetter the soil the lower the value.)
-  
+  sensorValue1 =analogRead(sensorPin1); //read sensor value (the wetter the soil the lower the value.)
+  sensorValue2 =analogRead(sensorPin2); //read sensor value (the wetter the soil the lower the value.)
+  sensorValue3 =analogRead(sensorPin3); //read sensor value (the wetter the soil the lower the value.)
+  sensorValue4 =analogRead(sensorPin4); //read sensor value (the wetter the soil the lower the value.)
   }
  
 void setup() {
   Serial.begin(115200);
-  pinMode(sensorPin, INPUT);
+  pinMode(sensorPin1, INPUT);
+  pinMode(sensorPin2, INPUT);
+  pinMode(sensorPin3, INPUT);
+  pinMode(sensorPin4, INPUT);
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
@@ -72,9 +85,10 @@ void loop() {
   readsensor();
   // Set values to send
   myData.id = id;
-  myData.x = sensorValue;
-  //myData.y = 0;
-
+  myData.h1 = sensorValue1;
+  myData.h2 = sensorValue2;
+  myData.h3 = sensorValue3;
+  myData.h4 = sensorValue4;
   // Send message via ESP-NOW
   esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
    
